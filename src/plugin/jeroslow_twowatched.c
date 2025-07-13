44597d483a9a834576d7224bdb5db0e7591f5264
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-07-13 00:44:29 -0300

    Ordenação do vetor de scores

--- src/plugin/jeroslow_twowatched.c
+++ src/plugin/jeroslow_twowatched.c
@@ -4,91 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct VarScore
+{
+    int var_index;
+    double score;
+    bool best_value;
+} VarScore;
+
 double *scores = NULL;
+VarScore *sorted_vars = NULL;
 
-typedef struct TwoWatchedClause 
+int compareVarScore(const void *a, const void *b)
 {
-    int ids[2];
-    Clause *c;
-} TwoWatchedClause;
+    const VarScore *va = (const VarScore *)a;
+    const VarScore *vb = (const VarScore *)b;
 
-typedef struct TwoWatchedLiterals 
-{
-    TwoWatchedClause *c;
-    struct TwoWatchedLiterals *next;
-} TwoWatchedLiterals;
-
-TwoWatchedLiterals **twl = NULL;
-TwoWatchedClause *twc = NULL;
-
-TwoWatchedLiterals* push(TwoWatchedLiterals *list, TwoWatchedClause *c)
-{
-    TwoWatchedLiterals *new = (TwoWatchedLiterals *) malloc(sizeof(TwoWatchedLiterals));
-    new->c = c;
-    new->next = list;
-    return new;
-}
-
-TwoWatchedLiterals* del(TwoWatchedLiterals *head, TwoWatchedClause *target)
-{
-    if (head == NULL) return NULL;
-
-    if (head->c == target)
-    {
-        TwoWatchedLiterals *next = head->next;
-        free(head);
-        return next;
-    }
-
-    TwoWatchedLiterals *prev = head;
-    TwoWatchedLiterals *curr = head->next;
-
-    while (curr != NULL)
-    {
-        if (curr->c == target)
-        {
-            prev->next = curr->next;
-            free(curr);
-            break;
-        }
-        prev = curr;
-        curr = curr->next;
-    }
-
-    return head;
-}
-
-bool find_and_move(TwoWatchedLiterals **list, TwoWatchedClause *tw_clause, int falseIndex, int pos)
-{
-    Clause *clause = tw_clause->c;
-    for (int i=0; i < clause->size; i++)
-        {
-            if (i == tw_clause->ids[0] || i == tw_clause->ids[1]) continue;
-
-            LiteralId candidate = clause->literals[i];
-
-            if (getLitState(candidate) != FALSE)
-            {
-                // troca pelo literal falsificado
-                tw_clause->ids[(falseIndex == tw_clause->ids[0])? 0 : 1] = i;
-
-                // Move a cláusula da lista do literal antigo para o novo
-                list[pos] = del(list[pos], tw_clause);
-
-                int newPos = getPos(candidate);
-                list[newPos] = push(list[newPos], tw_clause);
-
-                return true;
-            }
-        }
-        return false;
+    if (va->score < vb->score)
+        return 1;
+    else if (va->score > vb->score)
+        return -1;
+    else
+        return 0;
 }
 
 void PreProcessing(Form *form)
 {
-    // JEROSLOW
-
-    scores = (double *) calloc((form->numVars * 2), sizeof(double));
+    int num_vars = form->numVars;
+    scores = (double *)malloc(sizeof(double) * (num_vars * 2));
+    sorted_vars = (VarScore *)malloc(sizeof(VarScore) * num_vars);
 
     ClauseNode *list = form->clauses;
     while (list != NULL)
@@ -108,80 +51,45 @@ void PreProcessing(Form *form)
             }
             else
             {
-                scores[var_index + form->numVars] += score;
+                scores[var_index + num_vars] += score;
             }
         }
         list = list->next;
     }
 
-    // TWO WATCHED LITERALS
-
-    // aloca vetor de clausulas
-    twc = (TwoWatchedClause *) malloc(sizeof(TwoWatchedClause) * form->numClauses);
-    ClauseNode *twc_pivot = form->clauses;
-    for (int i=0; i < form->numClauses; i++)
+    for (int i = 0; i < num_vars; ++i)
     {
-        twc[i].c = twc_pivot->clause;
-        twc[i].ids[0] = 0;
-        twc[i].ids[1] = (twc[i].c->size > 1 ? 1 : 0);
-        twc_pivot = twc_pivot->next;
-    }
+        double pos_score = scores[i];
+        double neg_score = scores[i + num_vars];
 
-    // aloca vetor de literais
-    int numLiterals = form->numVars * 2;
-    twl = (TwoWatchedLiterals **) calloc(numLiterals, sizeof(TwoWatchedLiterals *));
-
-    // preenche
-    ClauseNode *head=form->clauses;
-    for (int i=0; i < form->numClauses; i++){
-        int w0 = twc[i].ids[0];
-        int w1 = twc[i].ids[1];
-
-        LiteralId l0 = twc[i].c->literals[w0];
-        twl[getPos(l0)] = push(twl[getPos(l0)], &twc[i]);
-
-        if (w0 != w1)
+        sorted_vars[i].var_index = i;
+        if (pos_score >= neg_score)
         {
-            LiteralId l1 = twc[i].c->literals[w1];
-            twl[getPos(l1)] = push(twl[getPos(l1)], &twc[i]);
+            sorted_vars[i].score = pos_score;
+            sorted_vars[i].best_value = TRUE;
+        }
+        else
+        {
+            sorted_vars[i].score = neg_score;
+            sorted_vars[i].best_value = FALSE;
         }
     }
+
+    qsort(sorted_vars, form->numVars, sizeof(VarScore), compareVarScore);
 }
 
 enum DecideState Decide(const Form *form)
 {
-
-    double max_score = -1.0;
-    int best_var = -1;
-    bool best_value = FALSE;
     int num_vars = form->numVars;
 
     for (int i = 0; i < num_vars; ++i)
     {
-        if (getVarState(i) != UNK)
-            continue;
-
-        double pos_score = scores[i];
-        double neg_score = scores[i + num_vars];
-
-        if (pos_score > max_score)
+        int var = sorted_vars[i].var_index;
+        if (getVarState(var) == UNK)
         {
-            max_score = pos_score;
-            best_var = i;
-            best_value = TRUE;
+            insertDecisionLevel(var, sorted_vars[i].best_value);
+            return FOUND_VAR;
         }
-        if (neg_score > max_score)
-        {
-            max_score = neg_score;
-            best_var = i;
-            best_value = FALSE;
-        }
-    }
-
-    if (best_var != -1)
-    {
-        insertDecisionLevel(best_var, best_value);
-        return FOUND_VAR;
     }
 
     return ALL_ASSIGNED;
@@ -189,63 +97,33 @@ enum DecideState Decide(const Form *form)
 
 bool BCP(Form *formula, const Decision decision)
 {
+    bool flag;
+    ClauseNode *head;
+    Clause *clause;
+
     LiteralId falseValuedLiteral = ((decision.value == FALSE) ? decision.id + 1 : -decision.id - 1);
-    int pos = getPos(falseValuedLiteral);
 
-    TwoWatchedLiterals *list = twl[pos];
+    head = formula->literals[getPos(falseValuedLiteral)];
 
-    while (list != NULL)
+    while (head != NULL)
     {
-        TwoWatchedClause *tw_clause = list->c;
-        Clause *clause = tw_clause->c;
+        flag = false;
+        clause = head->clause;
 
-        int watch0 = tw_clause->ids[0];
-        int watch1 = tw_clause->ids[1];
-
-        // verifica qual dos literais se tornou falso
-        int falseIndex = -1;
-
-        if (clause->literals[watch0] == falseValuedLiteral) falseIndex = watch0;
-        else if (clause->literals[watch1] == falseValuedLiteral) falseIndex = watch1;
-        else 
+        for (int i = 0; i < clause->size; ++i)
         {
-            list = list->next;
-            continue;
+            LiteralId lit = clause->literals[i];
+
+            if (getLitState(lit) != FALSE)
+                flag = true;
         }
 
-        int otherIndex = (falseIndex == watch0) ? watch1 : watch0;
-        LiteralId otherLit = clause->literals[otherIndex];
-
-        // Se o outro literal está TRUE, a cláusula está satisfeita
-        // talvez seja mais correto verificar se é diferente de FALSE
-        if (getLitState(otherLit) == TRUE) 
+        if (!flag)
         {
-            list = list->next;
-            continue;
-        }
-
-        // encontrar um novo literal
-        if (find_and_move(twl, tw_clause, falseIndex, pos))
-        {
-            list = list->next;
-            continue;
-        }
-        
-        // Nenhum outro literal pôde ser assistido
-        if (getLitState(otherLit) == UNK)
-        {
-            // bool value = (otherLit > 0);
-            // propagar
-            // insertDecisionLevel(abs(otherLit) -1, (otherLit > 0));
-            // setVarState(abs(otherLit) - 1, otherLit > 0 ? TRUE : FALSE);
-            // return false;
-        } else
-        {
-            // Conflito: os dois literais são FALSE
             return false;
         }
 
-        list = list->next;
+        head = head->next;
     }
 
     return true;
