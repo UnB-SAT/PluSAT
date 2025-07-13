2f8248d5669f40895f806622ede3b2a4c202fee5
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-07-13 00:37:57 -0300

    Implementação VSIDS

--- src/plugin/jeroslow_twowatched.c
+++ src/plugin/jeroslow_twowatched.c
@@ -1,37 +1,86 @@
 #include "dpll.h"
 #include "formula.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
+ClauseNode *learned_clauses = NULL;
+Clause *conflicting_clause_ptr = NULL;
+
+// A cada no máximo 100 conflitos a ativadade das variáveis decai
+#define DECAY_PERIOD 100
+
+static double var_inc = 1.0f;
+static double var_decay = 0.95f;
+static unsigned short conflicts_since_decay = 0;
+int num_variables = 0;
+double *activity = NULL;
+
 void PreProcessing(Form *form)
 {
+    ClauseNode *pivot, *head = form->clauses;
+
+    while (head != NULL)
+    {
+        pivot = head;
+
+        for (int j = 0; j < pivot->clause->size; ++j)
+        {
+            printf("%d ", pivot->clause->literals[j]);
+        }
+
+        head = head->next;
+        printf("\n");
+    }
+
     // Trabalhar estrutura de dados
+    num_variables = form->numVars;
+    activity = (double *)malloc(sizeof(double) * (num_variables + 1));
+    if (!activity)
+    {
+        perror("Falha ao alocar memória para as atividades das variáveis");
+        exit(1);
+    }
+
+    for (int i = 0; i < form->numVars; i++)
+        activity[i] = 0.0f;
 }
 
 enum DecideState Decide(const Form *form)
 {
-    // VSIDS
-    ClauseNode *list = form->clauses;
-    while (list != NULL)
+    ClauseNode *it = learned_clauses;
+    while (it != NULL)
     {
-
-        Clause *clause = list->clause;
-
+        Clause *clause = it->clause;
         for (int i = 0; i < clause->size; ++i)
         {
-            LiteralId lit = clause->literals[i];
-            // printf("Lit: %d\n", lit);
-            lit = ((lit > 0) ? lit : -lit); // Pega a variável
-            if (getVarState(lit - 1) == UNK)
-            {
-                insertDecisionLevel(lit - 1, FALSE);
+            LiteralId var_id = clause->literals[i];
+            var_id = ((var_id > 0) ? var_id : -var_id);
 
+            if (getVarState(var_id - 1) == UNK)
+            {
+                insertDecisionLevel(var_id - 1, FALSE); // Tenta o valor FALSE primeiro 
                 return FOUND_VAR;
             }
         }
+        it = it->next;
+    }
 
-        list = list->next;
+    double max_activity = -1.0;
+    int best_var = -1;
+
+    for (int i = 0; i < num_variables; i++)
+    {
+        if (getVarState(i) == UNK && activity[i] > max_activity)
+        {
+            max_activity = activity[i];
+            best_var = i;
+        }
+    }
+
+    if (best_var != -1)
+    {
+        insertDecisionLevel(best_var, FALSE);
+        return FOUND_VAR;
     }
 
     return ALL_ASSIGNED;
@@ -59,11 +108,15 @@ bool BCP(Form *formula, const Decision decision)
             LiteralId lit = clause->literals[i];
 
             if (getLitState(lit) != FALSE)
+            {
                 flag = true;
+                break;
+            }
         }
 
         if (!flag)
         {
+            conflicting_clause_ptr = clause;
             return false;
         }
 
@@ -75,6 +128,30 @@ bool BCP(Form *formula, const Decision decision)
 
 int resolveConflict()
 {
+    conflicts_since_decay++;
+    if (conflicting_clause_ptr != NULL)
+    {
+        // learned_clauses = addNodeOnList(conflicting_clause_ptr, learned_clauses)
+        for (int i = 0; i < conflicting_clause_ptr->size; i++)
+        {
+            LiteralId lit = conflicting_clause_ptr->literals[i];
+            int var_id = ((lit > 0) ? lit : -lit) - 1;
+
+            activity[var_id] += var_inc;
+        }
+
+        conflicting_clause_ptr = NULL;
+    }
+
+    if (conflicts_since_decay >= DECAY_PERIOD)
+    {
+        conflicts_since_decay = 0;
+        for (int i = 0; i < num_variables; i++)
+        {
+            activity[i] /= var_decay;
+        }
+    }
+
     Decision *decisions = getDecisions();
 
     int i = getLevel() - 1;
