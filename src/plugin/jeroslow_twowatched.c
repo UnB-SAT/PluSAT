1876df9f8d124a85d167e11b9582117d0e4afb28
martinsglucas <martin5.luca5.univ@gmail.com>
2025-07-13 00:43:57 -0300

    usa two watched, mas faltando propagacao unitaria

--- src/plugin/jeroslow_twowatched.c
+++ src/plugin/jeroslow_twowatched.c
@@ -18,6 +18,9 @@ typedef struct TwoWatchedLiterals
     struct TwoWatchedLiterals *next;
 } TwoWatchedLiterals;
 
+TwoWatchedLiterals **twl = NULL;
+TwoWatchedClause *twc = NULL;
+
 TwoWatchedLiterals* push(TwoWatchedLiterals *list, TwoWatchedClause *c)
 {
     TwoWatchedLiterals *new = (TwoWatchedLiterals *) malloc(sizeof(TwoWatchedLiterals));
@@ -26,8 +29,60 @@ TwoWatchedLiterals* push(TwoWatchedLiterals *list, TwoWatchedClause *c)
     return new;
 }
 
-TwoWatchedLiterals **twl = NULL;
-TwoWatchedClause *twc = NULL;
+TwoWatchedLiterals* del(TwoWatchedLiterals *head, TwoWatchedClause *target)
+{
+    if (head == NULL) return NULL;
+
+    if (head->c == target)
+    {
+        TwoWatchedLiterals *next = head->next;
+        free(head);
+        return next;
+    }
+
+    TwoWatchedLiterals *prev = head;
+    TwoWatchedLiterals *curr = head->next;
+
+    while (curr != NULL)
+    {
+        if (curr->c == target)
+        {
+            prev->next = curr->next;
+            free(curr);
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+bool find_and_move(TwoWatchedLiterals **list, TwoWatchedClause *tw_clause, int falseIndex, int pos)
+{
+    Clause *clause = tw_clause->c;
+    for (int i=0; i < clause->size; i++)
+        {
+            if (i == tw_clause->ids[0] || i == tw_clause->ids[1]) continue;
+
+            LiteralId candidate = clause->literals[i];
+
+            if (getLitState(candidate) != FALSE)
+            {
+                // troca pelo literal falsificado
+                tw_clause->ids[(falseIndex == tw_clause->ids[0])? 0 : 1] = i;
+
+                // Move a cláusula da lista do literal antigo para o novo
+                list[pos] = del(list[pos], tw_clause);
+
+                int newPos = getPos(candidate);
+                list[newPos] = push(list[newPos], tw_clause);
+
+                return true;
+            }
+        }
+        return false;
+}
 
 void PreProcessing(Form *form)
 {
@@ -62,7 +117,6 @@ void PreProcessing(Form *form)
     // TWO WATCHED LITERALS
 
     // aloca vetor de clausulas
-    // TODO: verificar e tratar clausulas unitarias
     twc = (TwoWatchedClause *) malloc(sizeof(TwoWatchedClause) * form->numClauses);
     ClauseNode *twc_pivot = form->clauses;
     for (int i=0; i < form->numClauses; i++)
@@ -77,18 +131,19 @@ void PreProcessing(Form *form)
     int numLiterals = form->numVars * 2;
     twl = (TwoWatchedLiterals **) calloc(numLiterals, sizeof(TwoWatchedLiterals *));
 
-    // aloca listas encadeadas
-    for (int i=0; i < numLiterals; i++)
-    {
-        twl[i] = (TwoWatchedLiterals *) malloc (sizeof(TwoWatchedLiterals));
-    }
-
     // preenche
-    ClauseNode *pivot, *head=form->clauses;
+    ClauseNode *head=form->clauses;
     for (int i=0; i < form->numClauses; i++){
-        for (int j=0; j < 2; j++){
-            int pos = getPos(twc[i].c->literals[twc[i].ids[j]]); // pos = getPos(twc[i].c->literals[twc[i].ids[j]]);
-            twl[pos] = push(twl[pos], &twc[i]);
+        int w0 = twc[i].ids[0];
+        int w1 = twc[i].ids[1];
+
+        LiteralId l0 = twc[i].c->literals[w0];
+        twl[getPos(l0)] = push(twl[getPos(l0)], &twc[i]);
+
+        if (w0 != w1)
+        {
+            LiteralId l1 = twc[i].c->literals[w1];
+            twl[getPos(l1)] = push(twl[getPos(l1)], &twc[i]);
         }
     }
 }
@@ -134,33 +189,63 @@ enum DecideState Decide(const Form *form)
 
 bool BCP(Form *formula, const Decision decision)
 {
-    bool flag;
-    ClauseNode *head;
-    Clause *clause;
-
     LiteralId falseValuedLiteral = ((decision.value == FALSE) ? decision.id + 1 : -decision.id - 1);
+    int pos = getPos(falseValuedLiteral);
 
-    head = formula->literals[getPos(falseValuedLiteral)];
+    TwoWatchedLiterals *list = twl[pos];
 
-    while (head != NULL)
+    while (list != NULL)
     {
-        flag = false;
-        clause = head->clause;
+        TwoWatchedClause *tw_clause = list->c;
+        Clause *clause = tw_clause->c;
 
-        for (int i = 0; i < clause->size; ++i)
+        int watch0 = tw_clause->ids[0];
+        int watch1 = tw_clause->ids[1];
+
+        // verifica qual dos literais se tornou falso
+        int falseIndex = -1;
+
+        if (clause->literals[watch0] == falseValuedLiteral) falseIndex = watch0;
+        else if (clause->literals[watch1] == falseValuedLiteral) falseIndex = watch1;
+        else 
         {
-            LiteralId lit = clause->literals[i];
-
-            if (getLitState(lit) != FALSE)
-                flag = true;
+            list = list->next;
+            continue;
         }
 
-        if (!flag)
+        int otherIndex = (falseIndex == watch0) ? watch1 : watch0;
+        LiteralId otherLit = clause->literals[otherIndex];
+
+        // Se o outro literal está TRUE, a cláusula está satisfeita
+        // talvez seja mais correto verificar se é diferente de FALSE
+        if (getLitState(otherLit) == TRUE) 
         {
+            list = list->next;
+            continue;
+        }
+
+        // encontrar um novo literal
+        if (find_and_move(twl, tw_clause, falseIndex, pos))
+        {
+            list = list->next;
+            continue;
+        }
+        
+        // Nenhum outro literal pôde ser assistido
+        if (getLitState(otherLit) == UNK)
+        {
+            // bool value = (otherLit > 0);
+            // propagar
+            // insertDecisionLevel(abs(otherLit) -1, (otherLit > 0));
+            // setVarState(abs(otherLit) - 1, otherLit > 0 ? TRUE : FALSE);
+            // return false;
+        } else
+        {
+            // Conflito: os dois literais são FALSE
             return false;
         }
 
-        head = head->next;
+        list = list->next;
     }
 
     return true;
